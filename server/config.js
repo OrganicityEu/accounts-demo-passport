543ac4194ec8e71702ac9ef06aa87476755bb7c2
Dennis Boldt <boldt@itm.uni-luebeck.de>
2016-10-05 11:52:07 +0200

    Input + jwt.io links

--- server/config.js
+++ server/config.js
@@ -2,8 +2,8 @@ var ids = {
   oAuth2: {
     authorizationURL : 'https://accounts.organicity.eu/realms/organicity/protocol/openid-connect/auth',
     tokenURL : 'https://accounts.organicity.eu/realms/organicity/protocol/openid-connect/token',
-    clientID: 'demo',
-    clientSecret: '0b2cbd34-a05e-4104-b7ec-350ee81bb7eb',
+    clientID: 'c7759f3a-8cb0-4ec7-9d96-88c337b725e2',
+    clientSecret: '331bf386-b032-4f3d-a0ca-eb3682730467',
     callbackURL: 'http://localhost:3000/auth/oauth2/callback'
   }
 };
